f14c61b50a6d967d8ec9db2c42eada70c51b4e34
DimaMakovetsky <mister.chuppa@mail.ru>
2023-05-02 00:06:30 +0300

    feat: add bottom text

--- js/script.js
+++ js/script.js
@@ -10,85 +10,6 @@ const russianButtonsCaps = ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '
 const keyNames = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
 let isCapslock = false;
 let isEnglish;
-function checkLocalStorage() {
-  let lang = localStorage.getItem('lang');
-  if (lang){
-    if (lang === 'en') {
-      isEnglish = true;
-    } else {
-      isEnglish = false;
-    }
-  } else {
-    isEnglish=true;
-    localStorage.setItem('lang','en');
-
-  }
-  console.log(isEnglish);
-}
-function fillKeyboard(buttonsArray) {
-  let index = 0;
-  const allButtons = document.querySelectorAll('.button');
-  allButtons.forEach((el) => {
-    const buttonText = buttonsArray[index];
-    el.classList.add(keyNames[index]);
-    const span = el.querySelector('span');
-    span.innerText = buttonText;
-    if (buttonText === 'Backspace' || buttonText === 'RShift' || buttonText === 'Enter' || buttonText === 'RShift' || buttonText === 'LShift' || buttonText === 'CapsLock') {
-      el.classList.add('big-button', 'control-button');
-    }
-    if (buttonText === 'Tab' || buttonText === 'Del' || buttonText === 'RCtrl' || buttonText === 'LCtrl' || buttonText === 'CapsLock' || buttonText === 'Win' || buttonText === 'LAlt' || buttonText === 'RAlt') {
-      el.classList.add('control-button');
-    }
-    if (buttonText === 'Space') {
-      el.classList.add('space', 'control-button');
-    }
-    index += 1;
-  });
-}
-function createEverything() {
-  const mainSection = document.createElement('section');
-  mainSection.classList.add('main-section');
-  document.body.append(mainSection);
-  const textArea = document.createElement('textarea');
-  textArea.setAttribute('rows', '10');
-  textArea.setAttribute('cols', '75');
-  textArea.classList.add('textarea');
-  mainSection.append(textArea);
-  const keyboard = document.createElement('div');
-  keyboard.classList.add('keyboard');
-  mainSection.append(keyboard);
-  let index = 0;
-  for (let i = 0; i < 5; i += 1) {
-    const keyboardRow = document.createElement('div');
-    keyboardRow.classList.add('keyboard__row');
-    keyboard.append(keyboardRow);
-    let lastKey = false;
-    while (!lastKey) {
-      const keyboardButton = document.createElement('div');
-      keyboardRow.append(keyboardButton);
-      keyboardButton.classList.add('button');
-      const buttonValue = document.createElement('span');
-      keyboardButton.append(buttonValue);
-      if (englishButtons[index] === 'Backspace' || englishButtons[index] === 'Del' || englishButtons[index] === 'Enter' || englishButtons[index] === 'RShift' || englishButtons[index] === 'RCtrl') {
-        lastKey = true;
-      }
-      switch(englishButtons[index])
-      {
-        
-      }
-      index += 1;
-    }
-  }
-  checkLocalStorage();
-  if(isEnglish) {
-    fillKeyboard(englishButtons);
-  } else {
-    fillKeyboard(russianButtons);
-  }
-}
-
-createEverything();
-
 function getCursorPos(input) {
   if ('selectionStart' in input && document.activeElement === input) {
     return {
@@ -112,6 +33,39 @@ function setCursorPos(input, start, end) {
     rng.moveEnd('character', end - start);
     rng.select();
   }
+}
+function checkLocalStorage() {
+  const lang = localStorage.getItem('lang');
+  if (lang) {
+    if (lang === 'en') {
+      isEnglish = true;
+    } else {
+      isEnglish = false;
+    }
+  } else {
+    isEnglish = true;
+    localStorage.setItem('lang', 'en');
+  }
+}
+function fillKeyboard(buttonsArray) {
+  let index = 0;
+  const allButtons = document.querySelectorAll('.button');
+  allButtons.forEach((el) => {
+    const buttonText = buttonsArray[index];
+    el.classList.add(keyNames[index]);
+    const span = el.querySelector('span');
+    span.innerText = buttonText;
+    if (buttonText === 'Backspace' || buttonText === 'RShift' || buttonText === 'Enter' || buttonText === 'RShift' || buttonText === 'LShift' || buttonText === 'CapsLock') {
+      el.classList.add('big-button', 'control-button');
+    }
+    if (buttonText === 'Tab' || buttonText === 'Del' || buttonText === 'RCtrl' || buttonText === 'LCtrl' || buttonText === 'CapsLock' || buttonText === 'Win' || buttonText === 'LAlt' || buttonText === 'RAlt') {
+      el.classList.add('control-button');
+    }
+    if (buttonText === 'Space') {
+      el.classList.add('space', 'control-button');
+    }
+    index += 1;
+  });
 }
 function typeKey(eventKey, textarea) {
   const cursorPos = getCursorPos(textarea);
@@ -140,7 +94,6 @@ function deleteKey(textarea, isDelete) {
         textarea.value = output;
         setCursorPos(textarea, cursorPos.start, cursorPos.end);
       }
-
     } else {
       output = [textareaValue.slice(0, cursorPos.start - 1), textareaValue.slice(cursorPos.start)].join('');
       textarea.value = output;
@@ -150,10 +103,53 @@ function deleteKey(textarea, isDelete) {
     textarea.value = textarea.value.slice(0, -1);
   }
 }
+function createEverything() {
+  const mainSection = document.createElement('section');
+  mainSection.classList.add('main-section');
+  document.body.append(mainSection);
+  const textArea = document.createElement('textarea');
+  textArea.setAttribute('rows', '10');
+  textArea.setAttribute('cols', '75');
+  textArea.classList.add('textarea');
+  mainSection.append(textArea);
+  const keyboard = document.createElement('div');
+  keyboard.classList.add('keyboard');
+  mainSection.append(keyboard);
+  const textP=document.createElement("p");
+  textP.innerText="Created on Windows, language change is Ctrl+Alt";
+  mainSection.append(textP);
+  textP.classList.add("text-p");
+  let index = 0;
+  for (let i = 0; i < 5; i += 1) {
+    const keyboardRow = document.createElement('div');
+    keyboardRow.classList.add('keyboard__row');
+    keyboard.append(keyboardRow);
+    let lastKey = false;
+    while (!lastKey) {
+      const keyboardButton = document.createElement('div');
+      keyboardRow.append(keyboardButton);
+      keyboardButton.classList.add('button');
+      const buttonValue = document.createElement('span');
+      keyboardButton.append(buttonValue);
+      if (englishButtons[index] === 'Backspace' || englishButtons[index] === 'Del' || englishButtons[index] === 'Enter' || englishButtons[index] === 'RShift' || englishButtons[index] === 'RCtrl') {
+        lastKey = true;
+      }
+      index += 1;
+    }
+  }
+  checkLocalStorage();
+  if (isEnglish) {
+    fillKeyboard(englishButtons);
+  } else {
+    fillKeyboard(russianButtons);
+  }
+}
+
+createEverything();
+
 function switchLanguage() {
   isEnglish = !isEnglish;
-  if(isEnglish)
-  {
+  if (isEnglish) {
     localStorage.setItem('lang', 'en');
   } else {
     localStorage.setItem('lang', 'ru');
@@ -168,8 +164,7 @@ document.addEventListener('keydown', (event) => {
   const keyboardKey = document.querySelector(`.${event.code}`);
   if (keyboardKey) {
     keyboardKey.classList.add('hover');
-    console.log(keyboardKey);
-    let key=keyboardKey.querySelector('span').innerText;
+    const key = keyboardKey.querySelector('span').innerText;
     const textArea = document.querySelector('.textarea');
     if (document.activeElement === textArea) {
       event.preventDefault();

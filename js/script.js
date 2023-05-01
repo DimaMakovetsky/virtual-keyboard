8a0a260e28a65996177b0e278e4b2e7c74cf12d7
DimaMakovetsky <mister.chuppa@mail.ru>
2023-05-01 21:57:13 +0300

    feat: add backspace, delete and capslock

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@
 
 const englishButtons = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter', 'LShift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'RShift', 'LCtrl', 'Win', 'LAlt', 'Space', 'RAlt', '←', '↓', '→', 'RCtrl'];
 const englishButtonsShift = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Del', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter', 'LShift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '↑', 'RShift', 'LCtrl', 'Win', 'LAlt', 'Space', 'RAlt', '←', '↓', '→', 'RCtrl'];
+const englishButtonsCaps = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Del', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter', 'LShift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '↑', 'RShift', 'LCtrl', 'Win', 'LAlt', 'Space', 'RAlt', '←', '↓', '→', 'RCtrl'];
 const keyNames = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
+let isCapslock = false;
 function fillKeyboard(buttonsArray) {
   let index = 0;
   const allButtons = document.querySelectorAll('.button');
@@ -95,14 +97,30 @@ function typeKey(eventKey, textarea) {
     textarea.value += eventKey;
   }
 }
+function deleteKey(textarea, isDelete) {
+  const cursorPos = getCursorPos(textarea);
+  const textareaValue = textarea.value;
+  let output;
+  if (cursorPos.start !== undefined) {
+    if (isDelete) {
+      if (cursorPos.start !== 0) {
+        output = [textareaValue.slice(0, cursorPos.start), textareaValue.slice(cursorPos.start + 1)].join('');
+        textarea.value = output;
+        setCursorPos(textarea, cursorPos.start, cursorPos.end);
+      }
+    } else {
+      output = [textareaValue.slice(0, cursorPos.start - 1), textareaValue.slice(cursorPos.start)].join('');
+      textarea.value = output;
+      setCursorPos(textarea, cursorPos.start - 1, cursorPos.end - 1);
+    }
+  } else if (!isDelete) {
+    textarea.value = textarea.value.slice(0, -1);
+  }
+}
 document.addEventListener('keydown', (event) => {
   const keyboardKey = document.querySelector(`.${event.code}`);
   if (keyboardKey) {
-    // console.log(event);
     keyboardKey.classList.add('hover');
-    // setTimeout(() => {
-    //   keyboardKey.classList.remove('hover');
-    // }, 500);
     const textArea = document.querySelector('.textarea');
     if (document.activeElement === textArea) {
       event.preventDefault();
@@ -126,13 +144,21 @@ document.addEventListener('keydown', (event) => {
           break;
       }
     } else {
-      console.log(event.code);
       switch (event.code) {
         case 'Space':
           typeKey(' ', textArea);
           break;
         case 'Enter':
           typeKey('\n', textArea);
+          break;
+        case 'Tab':
+          typeKey('\t', textArea);
+          break;
+        case 'Backspace':
+          deleteKey(textArea, false);
+          break;
+        case 'Delete':
+          deleteKey(textArea, true);
           break;
         case 'ShiftRight':
         case 'ShiftLeft':
@@ -148,10 +174,19 @@ document.addEventListener('keyup', (event) => {
   const keyboardKey = document.querySelector(`.${event.code}`);
   if (keyboardKey) {
     if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
-      fillKeyboard(englishButtons);
-      keyboardKey.classList.remove('hover');
-    } else {
-      keyboardKey.classList.remove('hover');
+      if (isCapslock) {
+        fillKeyboard(englishButtonsCaps);
+      } else {
+        fillKeyboard(englishButtons);
+      }
+    } else if (event.code === 'CapsLock') {
+      isCapslock = !isCapslock;
+      if (isCapslock) {
+        fillKeyboard(englishButtonsCaps);
+      } else {
+        fillKeyboard(englishButtons);
+      }
     }
+    keyboardKey.classList.remove('hover');
   }
 });
